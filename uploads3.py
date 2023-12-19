9fb47f2a13b794a3cb4faa6649a4a8b7f701f73c
Nitin Bhore <nitinbhore3@gmail.com>
2023-12-19 08:48:28 +0530

    Update uploads3.py

--- uploads3.py
+++ uploads3.py
@@ -1,10 +1,13 @@
 import boto3
-s3Resource = boto3.resource('s3')
+
+s3_resource = boto3.resource('s3')
+
 def upload_file(file_name, s3_file_location):
     try:
-        s3Resource.meta.client.upload_file(file_name,'damaimlbkt',s3_file_location)
+        s3_resource.meta.client.upload_file(file_name, 'damaimlbkt', s3_file_location)
     except Exception as exp:
-        print('exp: ', exp)
-file_name="Handshake.zip"
-s3_file_location="images/batches/batch1/"
+        print('exp:', exp)
+
+file_name = "Handshake.zip"
+s3_file_location = "images/batches/batch1/"
 upload_file(file_name, s3_file_location)
